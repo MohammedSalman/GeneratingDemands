6e1162d6cff70c82268395c4b2d48dcafc680fc8
mohammed <mohammad_w2000@yahoo.com>
2018-06-09 06:11:52 -0400

    calculating the required time

--- GenerateDemands.py
+++ GenerateDemands.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import time
+
+
 def create_topology():
     g_temp = nx.Graph()
     # Don't need to add nodes separately.
@@ -20,6 +22,24 @@ def create_topology():
     return (g)
 
 
+def check_validity(alist):
+    #This function checks for two things:
+    #First: if the summation of cols and rows are less or equal max_flows.
+    #Secnd: if all the numbers in 2D array are greater than 0.
+    axis0 = alist.sum(axis=0)
+    axis1 = alist.sum(axis=1)
+    check0 = all([axis0[i] <= max_flows[i] and
+                  axis1[i] <= max_flows[i]
+                  for i in range(len(max_flows))])
+    check1 = all([alist[i, j] >= 0
+              for i in range(alist.shape[0])
+              for j in range(alist.shape[1])])
+    if not check0 or not check1:
+        print("Found invalid demands")
+        return False
+    return
+
+
 if __name__ == "__main__":
     g = create_topology()
 
@@ -27,45 +47,41 @@ if __name__ == "__main__":
     # print(outdeg)
 
     outdeg = g.out_degree()
-    #print(outdeg)
+    # print(outdeg)
     flow_value = nx.maximum_flow_value(g, 1, 2)
-    #print(flow_value)
+    # print(flow_value)
     flow_value = nx.maximum_flow_value(g, 3, 1)
-    #print(flow_value)
+    # print(flow_value)
     flow_value = nx.maximum_flow_value(g, 2, 5)
-    #print(flow_value)
+    # print(flow_value)
     flow_value = nx.maximum_flow_value(g, 6, 2)
-    #print(flow_value)
+    # print(flow_value)
     flow_value = nx.maximum_flow_value(g, 1, 5)
-    #print(flow_value)
+    # print(flow_value)
     # to_remove = [n for n in outdeg if outdeg[n] == 1]
     # g.remove_nodes_from(to_remove)
 
     nx.draw(g, with_labels=True)
     plt.draw()
-    #plt.show()
+    # plt.show()
 
     n_nodes = 14
     alist = np.zeros([n_nodes, n_nodes])
-    max_flows = [3] * n_nodes
+    max_flows = [1] * n_nodes
 
     tic = time.time()
-    for t in range(10000):
-
+    for t in range(1):
+        alist1DnoZeros = []
         for i in range(alist.shape[0]):
             for j in range(alist.shape[1]):
-                if i == j: continue
+                if i == j:
+                    continue
                 alist[i, j] = random.uniform(0.0,
                                              min(max_flows[j] - np.sum(alist[:i, j]),
                                                  max_flows[i] - np.sum(alist[i, :j])))
-
-
+                alist1DnoZeros.append(alist[i, j])
         #print(alist)
         #print("************************")
-        axis0 = alist.sum(axis=0)
-        axis1 = alist.sum(axis=1)
-        check = [axis0[i] < max_flows[i] and axis1[i] < max_flows[i] for i in range(len(max_flows))]
-        if not all(check):
-            print("One found")
-    print("Required time: ", time.time()-tic)
-
+        print(list(alist1DnoZeros))
+        check_validity(alist)
+    print("Required time: ", time.time() - tic)
