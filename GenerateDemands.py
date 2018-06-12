2a779e8adfa808e0aa4cd24db39e96c41a8c7b37
Mohammed Salman <mohammad_w2000@yahoo.com>
2018-06-11 21:04:54 -0400

    generating demands the new way

--- GenerateDemands.py
+++ GenerateDemands.py
@@ -56,8 +56,14 @@ def check_validity(alist):
     #This function checks for two things:
     #First: if the summation of cols and rows are less or equal max_flows.
     #Secnd: if all the numbers in 2D array are greater than 0.
+    max_flows = [2, 3, 2, 4, 1]
     axis0 = alist.sum(axis=0)
+
     axis1 = alist.sum(axis=1)
+    print("axis0: ", axis0)
+    print("Difference: ", [axis0[i] - max_flows[i] for i in range(len(max_flows))])
+    print("axis1: ", axis1)
+    print("Difference: ", [axis1[i] - max_flows[i] for i in range(len(max_flows))])
     check0 = all([axis0[i] <= max_flows[i] and
                   axis1[i] <= max_flows[i]
                   for i in range(len(max_flows))])
@@ -67,6 +73,7 @@ def check_validity(alist):
     if not check0 or not check1:
         print("Found invalid demands")
         return False
+
     return
 
 
@@ -99,7 +106,6 @@ if __name__ == "__main__":
                     'capacity': 2,
                     'used_capacity': 0
                 }
-
             },
             1: {
                 'group1': {
@@ -107,7 +113,6 @@ if __name__ == "__main__":
                     'capacity': 3,
                     'used_capacity': 0
                 }
-
             },
             2: {
                 'group1': {
@@ -115,7 +120,6 @@ if __name__ == "__main__":
                     'capacity': 2,
                     'used_capacity': 0
                 }
-
             },
             3: {
                 'group1': {
@@ -128,15 +132,13 @@ if __name__ == "__main__":
                     'capacity': 1,
                     'used_capacity': 0
                 }
-
             },
             4: {
                 'group1': {
-                    'reachable_nodes': [3],
+                    'reachable_nodes': [0, 1, 2, 3],
                     'capacity': 1,
                     'used_capacity': 0
                 }
-
             },
         },
         'incoming': {
@@ -146,7 +148,6 @@ if __name__ == "__main__":
                     'capacity': 2,
                     'used_capacity': 0
                 }
-
             },
             1: {
                 'group1': {
@@ -154,7 +155,6 @@ if __name__ == "__main__":
                     'capacity': 3,
                     'used_capacity': 0
                 }
-
             },
             2: {
                 'group1': {
@@ -162,7 +162,6 @@ if __name__ == "__main__":
                     'capacity': 2,
                     'used_capacity': 0
                 }
-
             },
             3: {
                 'group1': {
@@ -175,48 +174,83 @@ if __name__ == "__main__":
                     'capacity': 1,
                     'used_capacity': 0
                 }
-
             },
             4: {
                 'group1': {
-                    'reachable_nodes': [3],
+                    'reachable_nodes': [0, 1, 2, 3],
                     'capacity': 1,
                     'used_capacity': 0
                 }
-
-            },
+            }
         }
     }
 
     def return_capacity(node, direction):
         return residual_network[direction][node]['group1']['capacity']
 
+    def get_group_name(src, node):
+        for group in residual_network['outgoing'][src].keys():
+            if node_reachable(src, node, group):
+                return group
+        #raise "node should be reachable in one of the groups"
+
+        #return node in residual_network['outgoing'][src][group]['reachable_nodes']
+    def get_available_capacity(src, dst, direction):
+        group = get_group_name(src, dst)
+        #print(src, dst, group)
+        return residual_network[direction][src][group]['capacity'] - \
+               residual_network[direction][src][group]['used_capacity']
+    def update_used_capacity(src, dst, value):
+        for direction in residual_network.keys():
+            #print(direction)
+            group = get_group_name(src, dst)
+            #print(group)
+            used_capacity = residual_network[direction][src][group]['used_capacity']
+            capacity = residual_network[direction][src][group]['capacity']
+            residual_network[direction][src][group]['used_capacity'] = used_capacity + value
+
+
+
     print("returned capacity:", return_capacity(0, 'outgoing'))
     def node_reachable(src, node, group):
         return node in residual_network['outgoing'][src][group]['reachable_nodes']
     print("Checking if node 1 is reachable from 0: ", node_reachable(0, 1, 'group1'))
+
+
 
     nx.draw(g, with_labels=True)
     plt.draw()
     #plt.show()
 
     alist = np.zeros([n_nodes, n_nodes])
-    max_flows = [1] * n_nodes
+
 
     tic = time.time()
-    for t in range(10000):
+    for t in range(1):
         alist1DnoZeros = []
         for i in range(alist.shape[0]):
             for j in range(alist.shape[1]):
                 if i == j:
                     continue
-                alist[i, j] = \
+                min_cap_both_directions = min(get_available_capacity(i, j, 'outgoing'),
+                                              get_available_capacity(j, i, 'incoming'))
+                if max_flows_dict[(i, j)] <= min_cap_both_directions:
+                    alist[i, j] = random.uniform(0.0, max_flows_dict[i, j])
+                else:
+                    alist[i, j] = random.uniform(0.0, min_cap_both_directions)
+
+                #now update the used_capacity in both directions
+                update_used_capacity(i, j, alist[i, j])
+                '''alist[i, j] = \
                     random.uniform(0.0,
                                              min(max_flows[j] - np.sum(alist[:i, j]),
-                                                 max_flows[i] - np.sum(alist[i, :j])))
+                                                 max_flows[i] - np.sum(alist[i, :j])))'''
                 alist1DnoZeros.append(alist[i, j])
-        #print(alist)
+        print(alist)
+        check_validity(alist)
+        #if check_validity(alist):
+        #   print(alist1DnoZeros)
         #print("************************")
         #print(list(alist1DnoZeros))
-        #check_validity(alist)
+
     print("Required time: ", time.time() - tic)
